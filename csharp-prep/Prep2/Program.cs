1a5775381164f45267f6931bb67ad2b4b1a1c3cb
Your Name <efffectery@gmail.com>
2025-02-25 12:57:30 -0700

    This is the completed Prep#2 program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        int grade_percent = int.Parse(Console.ReadLine());
+
+        string letter = "N/A";
+
+        if (grade_percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade_percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade_percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade_percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is {letter}!");
+
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("You Pass!");
+        } 
+
+        else
+        {
+            Console.WriteLine("You Fail!");
+        }
+
     }
 }
