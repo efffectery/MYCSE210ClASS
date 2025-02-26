3d5d14eae72c3efce5752ccb2cff2593cf8d8572
Your Name <efffectery@gmail.com>
2025-02-26 09:53:01 -0700

    completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_num = randomGenerator.Next(1, 100);
+
+        bool done = false;
+        int i = 0;
+        while (done == false)
+        {
+            Console.Write("What is your Guess? ");
+            int user_guess = int.Parse(Console.ReadLine());
+            i++;
+
+            if (magic_num > user_guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_num < user_guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+                Console.WriteLine($"You Guess {i} times");
+                done = true;
+            }
+        }
     }
 }
