c4cc6a10431ec9133aed02f969da9697af7181ad
Your Name <efffectery@gmail.com>
2025-02-27 09:45:05 -0700

    This is the completed Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int user_num = PromptUserNumber();
+        int squared_num = SquareNumber(user_num);
+        DisplayResult(squared_num, name);
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string user_name = Console.ReadLine();
+        return user_name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your faviortie number? ");
+        int user_fav_num = int.Parse(Console.ReadLine());
+        return user_fav_num;
+    }
+
+    static int SquareNumber(int number_to_be_squared)
+    {
+        return number_to_be_squared * number_to_be_squared;
+    }
+
+    static void DisplayResult(int squared_num, string name)
+    {
+        Console.WriteLine($"{name}, the square root of your number is {squared_num}");
+    }
+
 }
