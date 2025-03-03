9699a59701cc0c63406d9f9c8d4363ca1d096e39
Benson Montano <efffectery@gmail.com>
2025-03-03 10:00:12 -0700

    Class Classes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,13 +1,63 @@
 using System;
+using System.IO.Compression;
+
+class Circle
+{
+    private double radius;
+
+    // Constuctor gets called everytime or used to init all created objects you make
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetArea()
+    {
+        double area = Math.PI * radius * radius;
+        return area;
+    }   
+
+    public double GetCircumfrance()
+    {
+        double Circumfrance = Math.PI * GetDiameter();
+        return Circumfrance;
+    }
+
+    public double GetDiameter()
+    {
+        double Diameter = radius * 2;
+        return Diameter;
+    }
+
+    public void SetRadius(double r)
+    {
+        radius = r;
+    }
+
+    public void DisplayValues()
+    {
+        Console.WriteLine(GetArea());
+        Console.WriteLine(GetCircumfrance());
+        Console.WriteLine(GetDiameter());
+    }
+
+}
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("Hello CSE 210!");
-        Console.WriteLine("Please enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine($"{name}");
+
+        Circle myCircle = new Circle(9);
+        //myCircle.DisplayValues();
+
+        for (int i = 0; i < 10; i++)
+        {
+            myCircle.SetRadius(i);
+            myCircle.DisplayValues();
+
+        }
+
     }
 }
