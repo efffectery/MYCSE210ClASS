2f98841167a228713b1842bc06136eecca6a288b
Effectery <efffectery@gmail.com>
2025-03-17 12:53:23 -0600

    Finished Learning04 inheritance task

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        WritingAssignment write = new WritingAssignment("bella", "Making sandwitch", "The art of sandwhich making");
+        Console.WriteLine(write.GetSummery());
+        Console.WriteLine(write.GetWritingInfo());
+        
+        MathAssignment math = new MathAssignment("Steve", "trig", "7.3", "8-22");
+        Console.WriteLine(math.GetSummery());
+        Console.WriteLine(math.GetHomeWorkList());
+
     }
 }
