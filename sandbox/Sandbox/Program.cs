5ab30684bdd2987cde70e3b3eebabbb93326ff6b
Effectery <efffectery@gmail.com>
2025-03-27 12:29:11 -0600

    normal update

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,13 +1,11 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
 
-       HourlyEmployee mike = new HourlyEmployee();
-
-       Console.WriteLine($"{mike.GetHobbies()}");
 
     }
 
